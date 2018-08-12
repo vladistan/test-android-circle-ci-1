92c6b65ac8ea819024635f33e63754ca06eb884d
Vlad <vlad@v-lad.org>
2018-08-12 14:46:26 -0700

    Fix tests

--- app/src/androidTest/java/com/piggybank/mobile/myapplication/LoginActivityTest.java
+++ app/src/androidTest/java/com/piggybank/mobile/myapplication/LoginActivityTest.java
@@ -7,6 +7,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
@@ -18,7 +22,12 @@ public class LoginActivityTest {
     @Test
     public void shouldBeTrue() throws Exception
     {
-        assertEquals( 1, 2);
+        assertEquals( 1, 1);
+    }
+
+    @Test
+    public void shouldHaveHelloLabel() {
+        onView(withId(R.id.hello)).check(matches(withText("Hello")));
     }
 
 }
